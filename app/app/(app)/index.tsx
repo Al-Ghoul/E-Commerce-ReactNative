d41fd814252aa21e2f2c55aeb4563c05f69b37e1
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-11-19 21:37:19 +0200

    feat: initial empty index

--- app/app/(app)/index.tsx
+++ app/app/(app)/index.tsx
@@ -1,19 +1,5 @@
-import { useSession } from "@/components/AuthContext";
 import { View } from "react-native";
-import { Text } from "~/components/ui/text";
 
 export default function Index() {
-  const { signOut } = useSession();
-
-  return (
-    <View className="flex-1 items-center justify-center">
-      <Text
-        onPress={() => {
-          signOut();
-        }}
-      >
-        Sign Out
-      </Text>
-    </View>
-  );
+  return <View className="flex-1"></View>;
 }
