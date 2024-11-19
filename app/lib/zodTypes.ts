157ecb68ca67e3ca74631ee1f644d5b3add8d6e7
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-11-19 09:32:23 +0200

    feat: add zod schemas

---
+++ app/lib/zodTypes.ts
@@ -0,0 +1,40 @@
+import {z} from "zod";
+
+export const RegisterInputSchema = z.object({
+  email: z
+    .string({
+      required_error: "Email is required",
+      invalid_type_error: "Email must be a string",
+    })
+    .email(),
+  password: z
+    .string({
+      required_error: "Password is required",
+      invalid_type_error: "Password must be a string",
+    })
+    .min(8, "Password must be at least 8 characters"),
+});
+
+
+export const RegisterInputClientSchema = RegisterInputSchema.extend({
+  confirmPassword: z
+    .string({
+      required_error: "Confirm Password is required",
+      invalid_type_error: "Confirm Password must be a string",
+    })
+    .min(8, "Password must be at least 8 characters"),
+}).superRefine((val, ctx) => {
+  if (val.password != val.confirmPassword)
+    ctx.addIssue({
+      code: "custom",
+      message: "Passwords do NOT match.",
+      path: ["confirmPassword"],
+    });
+});
+
+export type RegisterInputClientSchemaType = z.infer<
+  typeof RegisterInputClientSchema
+>;
+
+export type LoginInputClientSchemaType = z.infer<typeof RegisterInputSchema>;
+
