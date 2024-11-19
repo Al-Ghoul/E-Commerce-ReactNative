d0d0d8390e869629dc1cb897a39450bb2c793662
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-11-19 09:32:06 +0200

    feat: use rnr's Text component

--- app/app/(app)/index.tsx
+++ app/app/(app)/index.tsx
@@ -1,19 +1,19 @@
-import { useSession } from '@/components/AuthContext';
-import { Text, View } from 'react-native';
-
+import { useSession } from "@/components/AuthContext";
+import { View } from "react-native";
+import { Text } from "~/components/ui/text";
 
 export default function Index() {
   const { signOut } = useSession();
-  
+
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <View className="flex-1 items-center justify-center">
       <Text
         onPress={() => {
           signOut();
-        }}>
+        }}
+      >
         Sign Out
       </Text>
     </View>
   );
 }
-
