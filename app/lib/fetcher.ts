f45c7113cc90726eca9a0a1d06a878e117bfcd8d
Abdo .AlGhoul <Abdo.AlGhouul@gmail.com>
2024-12-31 07:22:44 +0200

    refactor: remove unused default headers param

--- app/lib/fetcher.ts
+++ app/lib/fetcher.ts
@@ -2,9 +2,6 @@ import xior, { merge } from "xior";
 
 export const xiorInstance = xior.create({
   baseURL: "http://192.168.1.3:3000/api",
-  headers: {
-    // put your common custom headers here
-  },
 });
 
 export function addAuthHeader(token: string | null) {
